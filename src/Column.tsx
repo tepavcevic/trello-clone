ef8ea8a46e26218700b508aa620949f48ad94ea1
tepavcevic <djordje.tepa@gmail.com>
2023-12-21 23:07:25 +0100

    Implement drag and drop in column with throttling

--- src/Column.tsx
+++ src/Column.tsx
@@ -2,7 +2,11 @@ import AddNewItem from "./components/AddNewButton";
 import Card from "./Card";
 import { useAppState } from "./hooks/useAppState";
 import { ColumnContainer, ColumnTitle } from "./styles";
-import { addTask } from "./context/actions";
+import { addTask, moveList } from "./context/actions";
+import { useRef } from "react";
+import useDragItem from "./hooks/useDragItem";
+import { useDrop } from "react-dnd";
+import { throttle } from "throttle-debounce-ts";
 
 type ColumnProps = {
   id: string;
@@ -10,12 +14,25 @@ type ColumnProps = {
 };
 
 export default function Column({ id, text }: ColumnProps) {
-  const { getTasksByListId, dispatch } = useAppState();
-
+  const { draggedItem, getTasksByListId, dispatch } = useAppState();
   const tasks = getTasksByListId(id);
+  const ref = useRef<HTMLDivElement>(null);
+  const { drag } = useDragItem({ type: "COLUMN", id, text });
+  const [, drop] = useDrop({
+    accept: "COLUMN",
+    hover: throttle(200, () => {
+      if (!draggedItem) return;
+      if (draggedItem.type === "COLUMN") {
+        if (draggedItem.id === id) return;
+        dispatch(moveList(draggedItem.id, id));
+      }
+    }),
+  });
+
+  drag(drop(ref));
 
   return (
-    <ColumnContainer>
+    <ColumnContainer ref={ref}>
       <ColumnTitle>{text}</ColumnTitle>
       {tasks.map((task) => (
         <Card key={id} id={id} text={task.text} />
